1335afa461eba5be2c6d80a42385350d37419001
stoncalkins@gmail.com <stoncalkins@gmail.com>
2022-01-27 13:53:17 -0500

    updated controllers

---
+++ controllers/users.js
@@ -0,0 +1,108 @@
+
+const {users} = require('../models');
+
+
+const usersController = {
+    
+    
+    createUsers({body}, res) {
+        users.create(body)
+        .then(dbUsersData => res.json(dbUsersData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    
+    getAllUsers(req, res) {
+        users.find({})
+        
+        .populate({path: 'thoughts', select: '-__v'})
+        
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        
+        .then(dbUsersData => res.json(dbUsersData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+   
+    getUsersById({params}, res) {
+        users.findOne({_id: params.id })
+        .populate({path: 'thoughts', select: '-__v'})
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+       
+        .then(dbUsersData => {
+            if(!dbUsersData) {
+                res.status(404).json({message: 'No User with this particular ID!'});
+                return; 
+            }
+            res.json(dbUsersData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        })
+    },
+
+    
+    updateUsers({params, body}, res) {
+        users.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
+        .then(dbUsersData => {
+            if(!dbUsersData) {
+                res.status(404).json({message: 'No User with this particular ID!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err))
+    },
+
+    deleteUsers({params}, res) {
+        users.findOneAndDelete({_id: params.id})
+        .then(dbUsersData => {
+            if(!dbUsersData) {
+                res.status(404).json({message: 'No User with this particular ID!'});
+                return;
+            }
+            res.json(dbUsersData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    
+    addFriend({params}, res) {
+        users.findOneAndUpdate({_id: params.id}, {$push: { friends: params.friendId}}, {new: true})
+        .populate({path: 'friends', select: ('-__v')})
+        .select('-__v')
+        .then(dbUsersData => {
+            if (!dbUsersData) {
+                res.status(404).json({message: 'No User with this particular ID!'});
+                return;
+            }
+        res.json(dbUsersData);
+        })
+        .catch(err => res.json(err));
+    },
+
+   
+    deleteFriend({ params }, res) {
+        users.findOneAndUpdate({_id: params.id}, {$pull: { friends: params.friendId}}, {new: true})
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        .then(dbUsersData => {
+            if(!dbUsersData) {
+                res.status(404).json({message: 'No User with this particular ID!'});
+                return;
+            }
+            res.json(dbUsersData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
+
+};
+
+// Export module users controller
+module.exports = usersController; 
