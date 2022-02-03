603c3c0dc602368570a42894c0df0bab829445dd
stoncalkins@gmail.com <stoncalkins@gmail.com>
2022-02-03 18:16:20 -0500

    updated code

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNetwork', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialNetwork', {
+
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
